5d4b3bda430f89c0f4c1fb95c2e4840508629881
Danilo Neves Cruz <cruzdanilo@gmail.com>
2017-06-20 17:09:17 -0300

    Fix action easing bug

--- src/Star.js
+++ src/Star.js
@@ -16,8 +16,8 @@ var Star = cc.Sprite.extend(/** @lends Star# */{
     this.setCascadeOpacityEnabled(true);
     this.setRotation(Math.random() * 360);
     this.setScale(0);
-    this.runAction(cc.scaleTo(Star.DEFAULT_ACTION_DURATION, 1)
-                     .easing(cc.easeBackOut()));
+    this.runAction(
+      new cc.EaseBackOut(cc.scaleTo(Star.DEFAULT_ACTION_DURATION,1)));
     this.runAction(cc.rotateBy(Star.ROTATION_PERIOD, 360).repeatForever());
   },
 
@@ -27,10 +27,10 @@ var Star = cc.Sprite.extend(/** @lends Star# */{
    * @param {function} [callback] - Function to call after animation
    */
   destroy: function(callback) {
-    this.runAction(cc.sequence(cc.scaleTo(Star.DEFAULT_ACTION_DURATION, 0)
-                                 .easing(cc.easeBackIn()),
-                               cc.callFunc(this.removeFromParent, this),
-                               cc.callFunc(callback)));
+    this.runAction(cc.sequence(
+      new cc.EaseBackIn(cc.scaleTo(Star.DEFAULT_ACTION_DURATION, 0)),
+      cc.callFunc(this.removeFromParent, this),
+      cc.callFunc(callback)));
   }
 });
 
